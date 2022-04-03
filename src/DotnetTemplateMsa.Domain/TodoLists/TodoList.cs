c0cb78745ccd7f5d0f0257de1532b108f972a560
juda-cit <juda@ciandt.com>
2022-04-03 18:52:44 -0300

    [hotfix] Corrected code smells (nullable use)

--- src/DotnetTemplateMsa.Domain/TodoLists/TodoList.cs
+++ src/DotnetTemplateMsa.Domain/TodoLists/TodoList.cs
@@ -6,8 +6,8 @@ public class TodoList
 {
     #region entity properties
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     #endregion
--- src/DotnetTemplateMsa.Domain/Todos/Todo.cs
+++ src/DotnetTemplateMsa.Domain/Todos/Todo.cs
@@ -6,8 +6,8 @@ public class Todo
 {
     #region entity properties
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
     public bool IsComplete { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
@@ -15,6 +15,6 @@ public class Todo
 
     #region entity relationships
     public int TodoListId { get; set; }
-    public TodoList TodoLists { get; set; }
+    public TodoList TodoLists { get; set; } = null!;
     #endregion
 }
