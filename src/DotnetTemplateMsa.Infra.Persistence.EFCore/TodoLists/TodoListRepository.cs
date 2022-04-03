7e15ee25d3da8b8a5a23e55f34740cf06f23b5d1
juda-cit <juda@ciandt.com>
2022-04-03 20:34:32 -0300

    [hotfix] Reduced lines by removing namespace keys

--- src/DotnetTemplateMsa.Infra.Persistence.EFCore/TodoLists/TodoListRepository.cs
+++ src/DotnetTemplateMsa.Infra.Persistence.EFCore/TodoLists/TodoListRepository.cs
@@ -2,15 +2,14 @@ using DotnetTemplateMsa.Domain.TodoLists;
 using DotnetTemplateMsa.Infra.Persistence.EFCore.Base;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotnetTemplateMsa.Infra.Persistence.EFCore.TodoLists
-{
-    public class TodoListRepository : BaseRepository, ITodoListRepository
-    {
-        public TodoListRepository(AppDbContext dbContext) : base(dbContext) { }
+namespace DotnetTemplateMsa.Infra.Persistence.EFCore.TodoLists;
 
-        public async Task<IEnumerable<TodoList>> SelectAsync()
-        {
-            return await _dbContext.TodoLists.ToListAsync();
-        }
+public class TodoListRepository : BaseRepository, ITodoListRepository
+{
+    public TodoListRepository(AppDbContext dbContext) : base(dbContext) { }
+
+    public async Task<IEnumerable<TodoList>> SelectAsync()
+    {
+        return await _dbContext.TodoLists.ToListAsync();
     }
 }
