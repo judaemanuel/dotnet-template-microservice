1eadc25ca2e2711d1780556e0b83933563a4e832
juda-cit <juda@ciandt.com>
2022-04-03 16:40:04 -0300

    [hotfix] Removed code smell (nullable prop)

--- src/DotnetTemplateMsa.Api/Categories/CategoryResource.cs
+++ src/DotnetTemplateMsa.Api/Categories/CategoryResource.cs
@@ -3,5 +3,5 @@ namespace DotnetTemplateMsa.Api.Categories;
 public class CategoryResource
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 }
